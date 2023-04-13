55c0efea79d25d9ec12d8d2f84ae8ed12e64935e
Jetlove19 <rbrorlove@gmail.com>
2023-04-13 12:44:07 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace FinalTask13
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        var watch = Stopwatch.StartNew();
+
+        List<string> list = new List<string>();
+        list = File.ReadLines("C:/Users/Jetlove/Downloads/Text1 (1).txt").ToList();
+            Console.WriteLine($"Вставка в List<T> заняла: {watch.Elapsed.TotalSeconds} мс");
+
+            LinkedList<string> linkedlist = new LinkedList<string>();
+            linkedlist.AddFirst(File.ReadAllText("C:/Users/Jetlove/Downloads/Text1 (1).txt"));
+            Console.WriteLine($"Вставка в LinkedList<T> заняла: {watch.Elapsed.TotalMilliseconds} мс");
+        }
+    }
+}
